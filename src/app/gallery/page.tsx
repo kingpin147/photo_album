1c2e321feb065f6a41b5a8423ee1922932d5e8bb
Muhammad Nouman Attique <nomiking0072012@gmail.com>
2024-03-01 23:32:01 +0500

    update images

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,39 +1,37 @@
-"use client";
-import { CldUploadButton } from "next-cloudinary";
-import { uploadResult } from "../page";
-import React from "react";
-import { Button } from "@/components/ui/button";
+import { CldImage } from "next-cloudinary";
+import UploadButton from "./upload-button";
+import cloudinary from "cloudinary";
+import { CloudinaryImage } from "./cloudinary-image";
 
-const GalleryPage = () => {
+type SearchResult = {
+  public_id: string;
+};
+
+const GalleryPage = async () => {
+  const results = (await cloudinary.v2.search
+    .expression("resource_type:image")
+    .sort_by("created_at", "desc")
+    .max_results(3)
+    .execute()) as { resources: SearchResult[] };
+
   return (
     <section>
-      <div className="flex justify-between">
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between">
         <h1 className="text-4xl font-bold">GalleryPage</h1>
-        <Button asChild>
-            <div className="flex gap-2"><svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5m-13.5-9L12 3m0 0 4.5 4.5M12 3v13.5"
+        <UploadButton />
+</div>
+        <div className="grid grid-cols-4 gap-4">
+          {results.resources.map((result: any) => (
+            <CloudinaryImage
+            key={result.public_id}
+            src={result.public_id}
+            width="400"
+            height="300"
+            // alt="an image of nothing"
             />
-          </svg>
-          
-
-          <CldUploadButton
-            onUpload={(result: uploadResult) => {
-              //   setImageId(result.info.public_id);
-            }}
-            uploadPreset="<ye26uyqh>"
-          />
-          </div>
-        </Button>
+          ))}
+        </div>
       </div>
     </section>
   );
